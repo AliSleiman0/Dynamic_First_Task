6831e2b9dc0591cde55d91322a3bdefc70e6cb24
Ali Sleiman <asleiman@acksession.com>
2025-08-26 15:13:32 +0300

    Structure Change

--- Backend_API/internal/models/Book.go
+++ Backend_API/internal/models/Book.go
@@ -3,8 +3,11 @@ package models
 type Book struct {
 	ID            int    `gorm:"primaryKey;autoIncrement" json:"id"`
 	Title         string `gorm:"size:255;not null" json:"title"`
-	Author        string `gorm:"size:255;not null" json:"author"`
 	PublishedYear int    `json:"published_year"`
 	Quantity      int    `json:"quantity"`
 	Genre         string `gorm:"size:100" json:"genre"`
+	Img_url       string `json:"img_url"`
+	
+	PublisherID   int    `json:"publisher_id"` // Foreign Key
+	Publisher     User   `gorm:"foreignKey:PublisherID" json:"publisher"`
 }
---
+++ Backend_API/internal/models/User.go
@@ -0,0 +1,17 @@
+package models 
+
+import (
+	"time"
+)
+//snake case for database and json data
+type User struct {
+    ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
+    FirstName string    `json:"first_name"`
+    LastName  string    `json:"last_name"`
+    Email     string    `json:"email"`
+    Password  string    `json:"-"` // hidden from JSON
+    CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+    UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+
+    BooksPublished []Book `gorm:"foreignKey:PublisherID" json:"books"`
+}
